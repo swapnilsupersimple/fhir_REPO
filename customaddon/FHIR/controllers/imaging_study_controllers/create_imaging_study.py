cb963ae147f68947237f110e4919fae836932590
swapnilsupersimple <swapnil.supersimplesoftware@gmail.com>
2022-01-14 17:11:45 +0530

    "changes in create_imaging_study.py"

--- customaddon/FHIR/controllers/imaging_study_controllers/create_imaging_study.py
+++ customaddon/FHIR/controllers/imaging_study_controllers/create_imaging_study.py
@@ -5,52 +5,41 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class ImagingStudy(http.Controller):
-
     @http.route('/create_imaging_study', type='json', auth='user')
     def create_imaging_study(self, **rec):
 
         if request.jsonrequest:
 
 
-            # vals={
-            #     "reference":[]
-            # }
-            # for inp_for_enpoint in rec["series"][0]["endpoint"]:
-            #     print("inp_for_series-->", inp_for_enpoint)
+
+            # values_for_imaging_study_series = []
             #
-            #
-            #     for key in vals:
-            #
-            #         if key == "reference":
-            #             vals["reference"].append(inp_for_enpoint)
+            values={
+                "description": "",
+                "endpoint": []
+            }
 
 
             values_for_imaging_study = {
-                "status":rec["status"],
-                "modality":[],
-                "note":[],
-                "procedureCode":[],
-                "numberOfSeries": rec["numberOfSeries"],
-                "numberOfInstances": rec["numberOfInstances"],
-                "description": rec["description"],
-                "started":rec["started"],
-                "reasonCode":[],
-                "endpoint":[],
-                "series":[ ]
+                "status": rec["status"],
+                "reasonCode": [],
+                "endpoint": [],
+                "series":[(0,0,values)]
+                # "series":[]
 
             }
 
-            #start_of_code_for_reason_code
+            # start_of_code_for_reason_code
 
             for inp_for_reasonCode in rec["reasonCode"][0]["coding"]:
-                print("inp_for_reasonCode-->",inp_for_reasonCode)
+                print("inp_for_reasonCode-->", inp_for_reasonCode)
 
                 for key in values_for_imaging_study:
 
                     if key == "reasonCode":
-                        values_for_imaging_study["reasonCode"].append((0,0,inp_for_reasonCode))
+                        values_for_imaging_study["reasonCode"].append((0, 0, inp_for_reasonCode))
 
-            #end_of_code_for_reason_code
+            # end_of_code_for_reason_code
 
             # start_of_code_for_endpoint
             for inp_for_endpoint in rec["endpoint"]:
@@ -59,60 +48,21 @@ class ImagingStudy(http.Controller):
                 for key in values_for_imaging_study:
 
                     if key == "endpoint":
-                        values_for_imaging_study["endpoint"].append((0, 0, inp_for_endpoint))
+                        values_for_imaging_study["endpoint"].append((0,0,inp_for_endpoint))
 
             # end_of_code_for_endpoint
 
-            # start_of_code_for_modality
-            for inp_for_modality in rec["modality"]:
-                print("inp_for_reasonCode-->", inp_for_modality)
-
+            for x in rec["series"][0]["endpoint"]:
                 for key in values_for_imaging_study:
+                        if key=="series":
+                            values["endpoint"].append((0,0,x))
 
-                    if key == "modality":
-                        values_for_imaging_study["modality"].append((0, 0, inp_for_modality))
+            y=rec["series"][0]["description"]
+            print(y)
 
-            # end_of_code_for_modality
-
-            # start_of_code_for_note
-            for inp_for_note in rec["note"]:
-                print("inp_for_reasonCode-->", inp_for_note)
-
-                for key in values_for_imaging_study:
-                    if key == "note":
-                            values_for_imaging_study["note"].append((0, 0, inp_for_note))
-
-            # end_of_code_for_note
-
-            for inp_for_procedureCode in rec["procedureCode"][0]["coding"]:
-                print("inp_for_reasonCode-->", inp_for_procedureCode)
-
-                for key in values_for_imaging_study:
-
-                    if key == "procedureCode":
-                        values_for_imaging_study["procedureCode"].append((0, 0, inp_for_procedureCode))
-
-
-            # for inp_for_endpoint in rec["series"]:
-            #
-            #     print("x-->",inp_for_endpoint)
-            #     for key in values_for_imaging_study:
-            #
-            #         if key == "series":
-            #             values_for_imaging_study["series"].append((0,0,inp_for_endpoint))
-
-            # for inp_for_series in rec["series"]:
-            #     inp_for_series.pop('endpoint')
-            #     print("inp_for_series-->",inp_for_series)
-            #
-            #     for key in values_for_imaging_study:
-            #
-            #         if key == "series":
-            #             values_for_imaging_study["series"].append((0, 0, inp_for_series))
-
-
-
-
+            for key in values_for_imaging_study:
+                    if key == "series":
+                        values["description"]=y
 
 
         new_imaging_study = request.env['imaging.study'].sudo().create(values_for_imaging_study)
@@ -120,9 +70,9 @@ class ImagingStudy(http.Controller):
         args = {'success': True, 'message': 'Success', 'id': new_imaging_study.id}
 
 
-        return  args
 
+        return args
 
-#time_date_format_for '%Y-%m-%d %H:%M:%S'
+# time_date_format_for '%Y-%m-%d %H:%M:%S'
 
-#extract dictionary from input dictionary , apppend to series.Do same for endpoint
+# extract dictionary from input dictionary , apppend to series.Do same for endpoint
